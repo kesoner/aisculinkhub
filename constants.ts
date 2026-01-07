00af042a096e44aed5f8451ee5628180bb5d686c
kesoner <kesoner666@gmail.com>
2026-01-07 23:59:12 +0800

    feat: add logo and update avatar image

--- constants.ts
+++ constants.ts
@@ -2,11 +2,12 @@ import { AppConfig } from './types';
 import { Instagram, Facebook, Mail, UserCog, Users, Globe } from 'lucide-react';
 
 export const CONFIG: AppConfig = {
-    avatarUrl: "https://images.unsplash.com/photo-1618005182384-a83a8bd57fbe?q=80&w=200&auto=format&fit=crop",
+    avatarUrl: "/asc_logo.png",
     title: "AISCU",
     subtitle: "人工智慧應用社",
     bio: "我們是東吳大學人工智慧應用社⚡<br/>社團內會舉辦工作坊、知識課程、競賽活動<br/>無論您是否有程式基礎都期待您的加入唷~🎉",
     email: "ai.scu.club@gmail.com",
+    logoUrl: "/asc_logo.png",
     socials: [
         { label: "Instagram", url: "https://www.instagram.com/ai.scu.club?igsh=d3BjbXZkOXV4aDVv", icon: Instagram },
         { label: "Facebook", url: "https://facebook.com", icon: Facebook },
